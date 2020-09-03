0165338f73cb9c7e76b51e8b79a00794b512aedc
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-03 15:15:32 -0500

    added audio to click for each button

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const buttons = document.querySelectorAll('.drum');
+const buttons = document.querySelectorAll('.key');
 
 buttons.forEach((button) => button.addEventListener('click', handleClick));
 
 function handleClick() {
-  var audio = new Audio('./dist/css/sounds/tom-1.mp3');
+  console.log(this);
+
+  const keyCode = this.getAttribute('data-key');
+  console.log(keyCode);
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  audio.currentTime = 0;
   audio.play();
 }
