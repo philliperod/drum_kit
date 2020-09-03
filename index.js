1105b296a46126ce35d7b874bf99383755061382
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-03 17:05:36 -0500

    tidy up comments

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// store all buttons with "key" class in a Node list and into one variable "buttons"
-const buttons = document.querySelectorAll('.key');
+const buttons = document.querySelectorAll('.key'); // store all buttons with "key" class in a Node list and into one variable "buttons"
 
-// for each value in a Node list assign an event with a "mouse click" and apply a constructor function
 buttons.forEach((button) => button.addEventListener('click', handleClick));
+// for each value in a Node list assign an event with a "mouse click" and apply a constructor function
 
 function handleClick() {
-  // store whatever data-key attribute into variable "keyCode"
   const keyCode = this.getAttribute('data-key');
-  // select audio element and corresponding stored variable "keyCode" and store it into variable "audio"
   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
   audio.currentTime = 0;
-  // play mp3 source file found with corresponding variable "audio" with its attribute data-key
   audio.play();
+
+  // store whatever data-key attribute into variable "keyCode"
+  // select audio element and corresponding stored variable "keyCode" and store it into variable "audio"
+  // play mp3 source file found with corresponding variable "audio" with its attribute data-key
 }
 
-// check doc for any key presses and assign an event listener
 document.addEventListener('keydown', handlePress);
+// check doc for any key presses and assign an event listener
 
 function handlePress(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
