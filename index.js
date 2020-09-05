ae8efb6e21f4c4e8ec49e74f1a76751b01e10e25
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-04 20:18:21 -0500

    added class for press effect for click and keydown; added functions for effects

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const buttons = document.querySelectorAll('.key'); // store all buttons with "key" class in a Node list and into one variable "buttons"
-
 buttons.forEach((button) => button.addEventListener('click', handleClick));
 // for each value in a Node list assign an event with a "mouse click" and apply a constructor function
 
@@ -8,15 +7,12 @@ function handleClick() {
   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
   audio.currentTime = 0;
   audio.play();
-  const clickAnimation = document.querySelector(`.key[data-key="${keyCode}"]`);
-  clickAnimation.classList.add('pressed');
-
-  // store the selected element with defined data-key into "keyCode" provided by click eventlistener
-  // store the audio element with defined data-key into variable "audio"
-  // keep play time of audio at zero and play
-  // store the selected "key" class with defined data-key into "keyAnimation"
-  // add "pressed" class (with CSS applied) to variable keyAnimation which holds selected element
 }
+// store the selected element with defined data-key into "keyCode" provided by click eventlistener
+// store the audio element with defined data-key into variable "audio"
+// keep play time of audio at zero and play
+// store the selected "key" class with defined data-key into "keyAnimation"
+// add "pressed" class (with CSS applied) to variable keyAnimation which holds selected element
 
 document.addEventListener('keydown', handlePress);
 // check doc for any key presses and assign an event listener
@@ -27,14 +23,15 @@ function handlePress(event) {
   audio.play();
   const keyAnimation = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   keyAnimation.classList.add('pressed');
-
-  // store the selected element with defined data-key into "audio" provided by input given from "keydown" eventListener
-  // keep play time of audio at zero and play
-  // store the selected "key" class with defined data-key into "keyAnimation"
-  // add "pressed" class (with CSS applied) to variable keyAnimation which holds selected element
 }
+// store the selected element with defined data-key into "audio" provided by input given from "keydown" eventListener
+// keep play time of audio at zero and play
+// store the selected "key" class with defined data-key into "keyAnimation"
+// add "pressed" class (with CSS applied) to variable keyAnimation which holds selected element
 
-// function buttonAnimation(currentKey) {
-//   const activeButton = document.querySelector(`button[data-key="${currentKey.keyCode}"]`);
-//   activeButton.classList.add('pressed');
-// }
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removePressed));
+
+function removePressed(event) {
+  this.classList.remove('pressed');
+}
